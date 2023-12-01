1f462b8f51b24a611a00d093c0f9d32d781804c1
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-12-01 10:00:10 +0100

    tasks 1 - 5

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,8 +4,8 @@ export default function handleProfileSignup() {
   let details = '';
   uploadPhoto()
     .then((photo) => { details += photo.body; })
-    .catch(() => console.log('Signup system offline'));
+    .catch((error) => console.log('Signup system offline'));
   createUser()
     .then((user) => console.log(`${details} ${user.firstName} ${user.lastName}`))
-    .catch(() => console.log('Signup system offline'));
+    .catch((error) => console.log('Signup system offline'));
 }
