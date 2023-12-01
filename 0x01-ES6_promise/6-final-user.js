4deb4c93765b1c487b48f706401b1b7dd3ca2469
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-12-01 11:57:20 +0100

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,7 +5,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   let arr = [];
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((result) => result.forEach((val) => {
-      let arr = []
       if (val.status === 'fullfiled') arr.push(v);
       else arr.push({ status: val.status, value: val.reason.message });
       return arr;
