e8cfcd6dcc0cdee46dd7749e22f42ebf3fbce9e2
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-12-03 09:31:12 +0100

    task 7

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,9 +1,3 @@
 export default function getBudgetObject(incom, Gdp, capital) {
-  const budget = {
-    income: incom,
-    gdp: Gdp,
-    capita: capital,
-  };
-
-  return budget;
+  return { income, gdp, capita};
 }
