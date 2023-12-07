a601bbf885391386e3d8a20426820e3bb3779dcd
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-12-07 15:59:15 +0100

    task 10

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export default function updateUniqueItems(mapItems) {
+  try {
+    for (const [k, v] of mapItems) {
+      if (v === 1) {
+        mapItems.set(k, 100);
+     }
+    }
+  } catch (err) {
+      throw new Error('Cannot process');
+  }
+}
