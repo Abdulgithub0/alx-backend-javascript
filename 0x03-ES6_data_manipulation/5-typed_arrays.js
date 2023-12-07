13def731b3f653ae0bcc2768bb470ae829ff64ed
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-12-07 13:50:20 +0100

    working typeArray

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,2 +1,10 @@
-export default function createInt8TypedArray() {
-  return ArrayBuffer()
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+  const buff = new ArrayBuffer(length);
+  const int8view = new Int8Array(buff);
+  int8view[position] = value;
+  return buff;
+}
+console.log(createInt8TypedArray(10, 2, 89));
