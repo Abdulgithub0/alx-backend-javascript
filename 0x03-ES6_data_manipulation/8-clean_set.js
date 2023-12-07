8dca47bc743eeb357ffe8a8dbeb5dea0c429209f
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-12-07 15:41:29 +0100

    task 9

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,8 @@
 export default function cleanSet(set, startString) {
-  if (!startString || instanceof(startString, String) {
+  if (!startString) {
     return '';
   }
   return [...set].filter((ele) => ele.startsWith(startString))
     .map((ele) => ele.slice(startString.length))
     .join('-');
 }
-
-console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
-console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 6));
