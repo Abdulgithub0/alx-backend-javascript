0c0d3ce902b80b914773779bd5f68adcfa52279d
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-12-07 15:25:00 +0100

    working set and string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,11 @@
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== String) {
+  if (!startString || instanceof(startString, String) {
     return '';
   }
   return [...set].filter((ele) => ele.startsWith(startString))
     .map((ele) => ele.slice(startString.length))
     .join('-');
 }
+
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 6));
