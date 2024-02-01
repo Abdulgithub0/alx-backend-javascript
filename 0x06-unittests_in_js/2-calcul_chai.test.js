4015a7dfde1331b9b9b7d32aca3a18d409e28931
Abdulgithub0 <belloabdulsamad26@gmail.com>
2024-02-01 12:16:31 +0100

    test 3

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,23 +1,47 @@
-const assert = require('assert');
-const calculateNumber = require("./1-calcul.js");
+const expect = require('chai').expect;
+const calculateNumber = require("./2-calcul_chai.js");
 
-describe('Test cases for CalculateNumber', () => {
-	it('test calculateNumber when type === SUM', () => {
-	   assert.equal(calculateNumber('SUM', 10.7, 11), 22);
-	   assert.equal(calculateNumber('SUM', 10, 11), 21);
-	   assert.equal(calculateNumber('SUM', 2.5, 3.5), 7);
+describe('Test cases for CalculateNumber when type === SUM', () => {
+	it('test calculateNumber int and float args', () => {
+	   expect(calculateNumber('SUM', 10.7, 11)).to.equal(22);
 	});
 
-	it('test calculateNumber when type === SUBTRACT', () => {
-	   assert.equal(calculateNumber('SUBTRACT', 10.7, 11), 0);
-	   assert.equal(calculateNumber('SUBTRACT', 10, 11), -1);
-	   assert.equal(calculateNumber('SUBTRACT', 4.5, 3.5), 1);
+	it('test calculateNumber int and float args', () => {
+	   expect(calculateNumber('SUM', 10, 11)).to.equal(21);
 	});
 
-	it('test calculateNumber when type == DIVIDE', () => {
-	   assert.equal(calculateNumber('DIVIDE', 11.5, 2.5), 4);
-	   assert.equal(calculateNumber('DIVIDE', 16, 8), 2);
-	   assert.equal(calculateNumber('DIVIDE', 20.9, 7), 3);
-	   assert.equal(calculateNumber('DIVIDE', 100, 0), 'Error');
+	it('test calculateNumber float and float args', () => {
+	   expect(calculateNumber('SUM', 2.5, 3.5)).to.equal(7);
 	});
+});
+
+describe('Test cases for CalculateNumber when type === SUBTRACT', () => {
+	it('test for float and int args', () => {
+	   expect(calculateNumber('SUBTRACT', 10.7, 11)).to.equal(0);
+	});
+
+	it('test for int and int args', () => {
+	   expect(calculateNumber('SUBTRACT', 10, 11)).to.equal(-1);
+	 });
+
+	it('test for float and float args', () => {
+	   expect(calculateNumber('SUBTRACT', 4.5, 3.5)).to.equal(1);
+	})
+});
+
+describe('test calculateNumber when type == DIVIDE', () => {
+	it('test for float and float args', () => {
+	   expect(calculateNumber('DIVIDE', 11.5, 2.5)).to.equal(4);
+	 });
+
+	it('test for int and it args',  () => {
+	   expect(calculateNumber('DIVIDE', 16, 8)).to.equal(2);
+	});
+
+	it('test for float and 7', () => {
+	   expect(calculateNumber('DIVIDE', 20.9, 7)).to.equal(3);
+	})
+	it('test for divisibility by zero', () => {
+	   expect(calculateNumber('DIVIDE', 100, 0)).to.equal('Error');
+	})
 })
